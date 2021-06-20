44d59c90ba3b018ca3c01aef9ce7e12c36a1ff5b
imborhade <sandeephborhade@gmail.com>
2021-06-20 23:39:44 +0530

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace UserAPI.Models
+{
+    public class User
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
