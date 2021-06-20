fe3556d5cd96b4262b7595b393d94af445827303
imborhade <sandeephborhade@gmail.com>
2021-06-20 23:55:14 +0530

    Update and rename WeatherForecastController.cs to UserController.cs

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserAPI.Models;
+
+namespace UserAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        private readonly UserContext _context;
+
+        public UserController(ILogger<UserController> logger, UserContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }        
+        
+        
+        // POST: api/user
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(User), new { phone = user.Phone }, user);
+        }
+ 
+ 
+    }
+}
